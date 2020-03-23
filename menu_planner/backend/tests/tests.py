5c0ea458dcbb2fa62d099d008ec8b898fc51ca8c
emoranska <emilia.moranska@gmail.com>
2020-03-23 17:17:16 +0100

    Test for recipe

--- menu_planner/backend/tests/tests.py
+++ menu_planner/backend/tests/tests.py
@@ -4,6 +4,8 @@ from backend.forms import SignUpForm
 
 
 #################### USER TESTS ####################
+from backend.models import Recipe
+
 
 @pytest.mark.django_db
 def test_register_user_ok():
@@ -70,38 +72,15 @@ def test_change_password(client):
     assert response.url == '/password_change_done/'
 
 
-
-# def test_change_password_difference():
-#     form_data = {'new_password': '#3AdcTu', 'new_password2': '#3Au'}
-#     form = SignUpForm(data=form_data)
-#     assert False == form.is_valid()
-#
-#
-# @pytest.mark.django_db
-# def test_get_recipe_list(client, set_up):
-#     response = client.get('/menuplanner/')
-#     assert response.status_code == 200
-#
-#
-# @pytest.mark.django_db
-# def test_get_recipe_detail(client, set_up):
-#     response = client.get('/menuplanner/recipe/1/', {})
-#     assert response.status_code == 200
-#     for field in ("name", "add_date", "ingredients", "description", "author"):
-#         assert field in response.data
-
-#
-# @pytest.mark.django_db
-# def test_get_shoppinglists(client, set_up):
-#     user = User.objects.get(id=1)
-#     response = client.get(f'/menuplanner/shoppinglists/{user.id}/')
-#     assert response.status_code == 200
+@pytest.mark.django_db
+def test_create_recipe(client, user):
+    recipe = Recipe.objects.create(name="Test_recipe", add_date='2020-03-14', description='whatever',
+                                    author=user)
+    assert recipe
 
 
+@pytest.mark.django_db
+def test_get_recipe_list(client):
+    response = client.get('/menuplanner/')
+    assert response.status_code == 200
 
-
-# @pytest.mark.django_db
-# def test_change_passwd_false():
-#     # form_data = {'password1': '#3AdcTu', 'password2': '#3Au'}
-#     form = SignUpForm(password1='#3AdcTu', password2='#3Au')
-#     assert False == form.is_valid()
